376237b0f0e527480aa21f2d4f141e6ced112ccd
huijeong365 <sarahcho87@gmail.com>
2024-01-11 17:44:51 +0900

    sec04/exam05 update

--- chap06/src/sec04/exam05/Car.java
+++ chap06/src/sec04/exam05/Car.java
@@ -18,7 +18,7 @@ public class Car {
 	void run() {
 		for(int i=10; i<=50; i+=10) {
 			speed = i;
-			System.out.println("달립니다.(시속:" + speed + "km/h");
+			System.out.println("달립니다.(시속:" + speed + "km/h)");
 		}
 	}
 }
