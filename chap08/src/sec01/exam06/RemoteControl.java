1edea4dbb35556ddd623ef0826efd7e6d7e8217e
huijeong365 <sarahcho87@gmail.com>
2024-01-19 17:34:42 +0900

    sec01.exam06

---
+++ chap08/src/sec01/exam06/RemoteControl.java
@@ -0,0 +1,12 @@
+package sec01.exam06;
+
+public interface RemoteControl {
+	//상수
+	int MAX_VOLUME = 10;		//static final 이  생략되어 있다.
+	int MIN_VOLUME = 0;
+	
+	//추상 메소드 		
+	void turnOn();				//public abstract 가 생략되어 있다.
+	void turnOff();
+	void setVolume(int volume);
+}
