c8b5ff8bf26d712822cdf94ae36aff47444b1d55
huijeong365 <sarahcho87@gmail.com>
2024-01-26 16:19:27 +0900

    exam07

---
+++ exam07/src/test01/ShopService.java
@@ -0,0 +1,14 @@
+package test01;
+
+public class ShopService {
+	
+	private static ShopService singleton = new ShopService();
+	
+	private ShopService() {}
+
+	public static ShopService getInstance() {
+		
+		return singleton;
+	}
+
+}
