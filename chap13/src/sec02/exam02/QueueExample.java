9e3919844fbb83aad53d428549a0762e77597a28
huijeong365 <sarahcho87@gmail.com>
2024-02-08 15:27:10 +0900

    chap13.sec02.exam02

---
+++ chap13/src/sec02/exam02/QueueExample.java
@@ -0,0 +1,32 @@
+package sec02.exam02;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+		Queue<Message> messageQueue = new LinkedList<Message>();
+		
+		messageQueue.offer(new Message("sendMail", "홍길동"));
+		messageQueue.offer(new Message("sendSMS", "오윤석"));
+		messageQueue.offer(new Message("sendkakaotalk", "홍두께"));
+		
+		while(!messageQueue.isEmpty()) {
+			Message message = messageQueue.poll();
+			switch(message.command) {
+			case "sendMail":
+				System.out.println(message.to + "님에게 메일을 보냅니다.");
+				break;
+			case "sendSMS":
+				System.out.println(message.to + "님에게 SMS을 보냅니다.");
+				break;
+			case "sendkakaotalk":
+				System.out.println(message.to + "님에게 카카오톡을 보냅니다.");
+				break;
+			}
+		}
+
+	}
+
+}
