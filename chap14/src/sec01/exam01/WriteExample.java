fa302c30119507bdf4d4b809caf2087564998626
huijeong365 <sarahcho87@gmail.com>
2024-02-13 16:22:09 +0900

    chap14.sec01.exam13

---
+++ chap14/src/sec01/exam01/WriteExample.java
@@ -0,0 +1,25 @@
+package sec01.exam01;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class WriteExample {
+
+	public static void main(String[] args) throws Exception {
+		OutputStream os = new FileOutputStream("C:/Temp/test1.db");
+		
+		byte a = 65;
+		byte b = 66;
+		byte c = 67;
+		
+		os.write(a);
+		os.write(b);
+		os.write(c);
+		
+		os.flush();
+		os.close();
+		
+
+	}
+
+}
---
+++ chap14/src/sec01/exam03/WriteExample.java
@@ -0,0 +1,21 @@
+package sec01.exam03;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class WriteExample {
+
+	public static void main(String[] args) throws Exception {
+		OutputStream os = new FileOutputStream("C:/Temp/test3.db");
+		
+		byte[] array = {65, 66, 67, 71, 76, 78, 90};
+		
+		os.write(array, 1 ,3);
+		
+		os.flush();
+		os.close();
+		
+
+	}
+
+}
